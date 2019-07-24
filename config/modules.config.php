cc84d9f37f109d66f0b726a0f13330083592dce6
comcduarte <33262448+comcduarte@users.noreply.github.com>
2019-07-24 16:10:47 -0400

    Implement Midnet Module

--- config/modules.config.php
+++ config/modules.config.php
@@ -11,7 +11,18 @@
  * This should be an array of module namespaces used in the application.
  */
 return [
+    'Zend\Form',
+    'Zend\InputFilter',
+    'Zend\Filter',
+    'Zend\Hydrator',
+    'Zend\Db',
+    'Zend\Paginator',
+    'Zend\I18n',
+    'Zend\Navigation',
+    'Zend\Mvc\Plugin\FlashMessenger',
+    'Zend\Session',
     'Zend\Router',
     'Zend\Validator',
     'Application',
+    'Midnet',
 ];
